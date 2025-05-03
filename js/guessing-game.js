6eb004589c7a15ca00a9f3e310355a0934d67dfe
FreeWifi0802 <104181615+FreeWifi0802@users.noreply.github.com>
2025-05-03 16:43:52 -0400

    Added game logic

--- js/guessing-game.js
+++ js/guessing-game.js
@@ -31,42 +31,90 @@ function shuffle(array) {
 }
 
 class Game {
+
+  playersGuess;
+  pastGuesses;
+  winningNumber;
+  maxNumGuesses = 5;
+
   constructor() {
     // Initialize properties:
     // - playersGuess (current guess)
     // - pastGuesses (array of previous guesses)
     // - winningNumber (generated number)
+    this.playersGuess = null;
+    this.pastGuesses = [];
+    this.winningNumber = generateWinningNumber();
   }
 
   // Return absolute difference between guess and winning number
   difference() {
     // Calculate and return difference
+    return Math.abs(this.winningNumber - this.playersGuess);
   }
 
   // Return true if guess is lower than winning number
   isLower() {
     // Return boolean comparison
+    return this.winningNumber - this.playersGuess > 0;
   }
 
   // Validate and process guess
   playersGuessSubmission(num) {
+    let tryNumber = Math.floor(parseInt(num));
+
+    if (isNaN(num) || num < 1 || num > 100){
+      throw "That is an invalid guess.";
+    }
+
+    this.playersGuess = tryNumber;
+    return this.checkGuess();
     // Throw error if invalid number
     // Set playersGuess
     // Return checkGuess result
   }
 
   // Evaluate guess and return feedback message
-  checkGuess() {
-    // Handle win condition
+  // Handle win condition
     // Handle duplicate guess
     // Add to pastGuesses
     // Handle max guesses
     // Return temperature feedback
+  checkGuess() {
+    if (this.playersGuess === this.winningNumber){
+      return "You Win!";
+    }
+
+    for (let i = 0; i < this.pastGuesses.length; i++){
+      if (this.playersGuess === this.pastGuesses[i]) {
+        return "You have already guessed that number.";
+      }
+    }
+
+    this.pastGuesses.push(this.playersGuess);
+
+    if (this.pastGuesses.length === this.maxNumGuesses) {
+      return "You Lose.";
+    }
+
+    if (Math.abs(this.winningNumber - this.playersGuess) < 10){
+      return "You're burning up!";
+    } else if (Math.abs(this.winningNumber - this.playersGuess) < 25){
+      return "You're lukewarm.";
+    } else if (Math.abs(this.winningNumber - this.playersGuess) < 50) {
+      return "You're a bit chilly.";
+    } else {
+      return "You're ice cold!";
+    }
   }
 
   // Generate array with 3 numbers (winning + 2 random)
   provideHint() {
     // Create array and shuffle
+    let hintArray = [this.winningNumber, generateWinningNumber(),
+      generateWinningNumber()];
+    hintArray = shuffle(hintArray);
+    return hintArray;
   }
 }
 
